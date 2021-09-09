a94362930c84422cce5899a658013925d6e22359
phil-all <allard.latour.philippe@gmail.com>
2021-09-09 10:47:57 +0200

    Define folder structure

--- config/config.php
+++ config/config.php
@@ -13,7 +13,21 @@ class Config
 
     private static function init()
     {
+        // constants definition
+        define('DS', DIRECTORY_SEPARATOR);
 
+        define('PUBLIC_PATH', ROOT .  DS . 'public' . DS);
+
+        define('CSS_PATH', PUBLIC_PATH . 'css' . DS);
+
+        define('JS_PATH', PUBLIC_PATH . 'js' . DS);
+
+        define('IMAGES_PATH', PUBLIC_PATH . 'images' . DS);
+
+        define('UPLOADS_PATH', PUBLIC_PATH . 'uploads' . DS);
+
+        // session starting
+        session_start();
     }
 
     private static function autoload()
