37e7e834030b88a23dfc31ebeaeff1d346ae3093
phil-all <allard.latour.philippe@gmail.com>
2021-11-09 23:43:37 +0100

    :sparkles: New class to centralize form tests methods #88

---
+++ libraries/formTest.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace Over_Code\Libraries;
+
+/**
+ * Contain methods used to check forms
+ */
+class FormTest
+{
+    use \Over_Code\Libraries\Helpers;
+
+    /**
+     * Cheks datas from registration form
+     *
+     * @return boolean
+     */
+    public function registerTest(): bool
+    {
+        return (
+            $this->isNameValid($this->getPOST('first_name')) &&
+            $this->isNamevalid($this->getPOST('last_name')) &&
+            $this->isPseudoValid($this->getPOST('pseudo')) &&
+            $this->isMailValid($this->getPOST('email')) &&
+            $this->isPassValid($this->getPOST('password')) &&
+            $this->isConfirmPassValid($this->getPOST('password'), $this->getPOST('confirm_password'))
+        );
+    }
+
+    /**
+     * Cheks datas from reset password form
+     *
+     * @return boolean
+     */
+    public function newPassTest(): bool
+    {
+        return (
+            $this->isPassValid($this->getPOST('password')) &&
+            $this->isConfirmPassValid($this->getPOST('password'), $this->getPOST('confirm_password'))
+        );
+    }
+
+    /**
+     * Checks if a name is valid.
+     * Name have to contain between 1 and 32 characters, as follow :
+     * - at least 1 uppercase letter
+     * - 0 or more lowercase letter
+     * - 0 or more space
+     * - 0 or more dash
+     * - 0 or more single quote
+     *
+     * @param string $name
+     *
+     * @return boolean
+     */
+    public function isNameValid(string $name): bool
+    {
+        return preg_match('~^(?=.*[A-Z])[a-zA-z\s\-\']+$~', $name);
+    }
+
+    /**
+     * Checks if a pseudo is valid.
+     * Pseudo have to contain between 8 and 32 characters, as follow :
+     * - at least 4 letters (lowercase or uppercase)
+     * - 0 or more digit
+     * - 0 or more underscore
+     *
+     * @param string $pseudo
+     *
+     * @return boolean
+     */
+    public function isPseudoValid(string $pseudo): bool
+    {
+        return preg_match('~^(?=.*[a-zA-Z]{4,})[\w]{8,32}$~', $pseudo);
+    }
+
+    /**
+     * Checks if an email address is valid
+     *
+     * @param string $email
+     *
+     * @return boolean
+     */
+    public function isMailValid(string $email): bool
+    {
+        return filter_var($email, FILTER_VALIDATE_EMAIL);
+    }
+
+    /**
+     * Checks if a password is valid.
+     * Password have to contain between 8 and 50 characters, as follow :
+     * - at least 1 lowercase
+     * - at least 1 upper case
+     * - at least one digit
+     * - at least 1 special character in the list : !@#$%-+
+     *
+     * @param string $pass
+     *
+     * @return boolean
+     */
+    public function isPassValid(string $pass): bool
+    {
+        return preg_match('~^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%\-+])[\w!@#$%\-+]{8,50}$~', $pass);
+    }
+
+    /**
+     * Checks if password is well confirmed
+     *
+     * @param string $pass
+     * @param string $confirmPass
+     *
+     * @return boolean
+     */
+    public function isConfirmPassValid(string $pass, string $confirmPass): bool
+    {
+        return ($pass === $confirmPass);
+    }
+}
