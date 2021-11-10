39e625113562c9667f4f608d7d09370c2f796a1e
phil-all <allard.latour.philippe@gmail.com>
2021-11-10 23:02:57 +0100

    :arrow_up: Update Email class #94

--- libraries/email.php
+++ libraries/email.php
@@ -16,7 +16,7 @@ class Email
     /**
      * Send an HTML email
      *
-     * @param array $reciever address(es) targeted by this email, can be string or array if multiple
+     * @param array $reciever address(es) targeted by this email
      * @param string $title email title
      * @param string $body html email source code
      *
@@ -34,6 +34,34 @@ class Email
             ->setFrom(['team.overcode@example.com' => 'Over_code Team'])
             ->setTo($reciever)
             ->setBody($body, 'text/html');
+
+        $mailer->send($message);
+    }
+
+    /**
+     * Send a text email from contact form to site admin
+     *
+     * @param string $firstName
+     * @param string $lastName
+     * @param string $email
+     * @param string $subject
+     * @param string $content
+     * 
+     * @return void
+     */
+    public function sendTextEmail(string $firstName, string $lastName, string $email, string $subject, string $content): void
+    {
+        $transport = (new Swift_SmtpTransport($this->getENV('SMTP_SERVER'), $this->getENV('SMTP_PORT')))
+            ->setUsername($this->getENV('SMTP_USERNAME'))
+            ->setPassword($this->getENV('SMTP_PASSWORD'));
+
+        $mailer = new Swift_Mailer($transport);
+
+        // Create a message
+        $message = (new Swift_Message($subject))
+        ->setFrom([$email => $firstName . ' ' . $lastName])
+        ->setTo(['receiver@domain.org', 'other@domain.org' => 'A name'])
+        ->setBody($content);
 
         $mailer->send($message);
     }
