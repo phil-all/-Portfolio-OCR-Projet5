df94d62a0a5935ccf8025d8b1f94d4cba2c534e8
phil-all <allard.latour.philippe@gmail.com>
2021-11-19 13:08:17 +0100

    :arrow_up: CRUD article : read article #112

--- controllers/admin/adminArticlesController.php
+++ controllers/admin/adminArticlesController.php
@@ -2,6 +2,7 @@
 
 namespace Over_Code\Controllers\Admin;
 
+use Over_Code\Models\CommentModel;
 use Over_Code\Models\ArticlesModel;
 use Over_Code\Models\CategoryModel;
 use Over_Code\Controllers\MainController;
@@ -18,7 +19,17 @@ class AdminArticlesController extends MainController
 {
     use \Over_Code\Libraries\Upload;
 
-    public function nouveau()
+    private array $articles;
+    private int $perPage = 4;
+    private int $totalPages;
+    private int $currentPage;
+
+    /**
+     * Sets params and template to twig, about create new article
+     *
+     * @return void
+     */
+    public function nouveau(): void
     {
         if ($this->userToTwig['admin']) {
             $category = new CategoryModel();
@@ -34,7 +45,12 @@ class AdminArticlesController extends MainController
         }
     }
 
-    public function post()
+    /**
+     * Processes article creation from form post
+     *
+     * @return void
+     */
+    public function post(): void
     {
         if ($this->userToTwig['admin']) {
             $upload = $this->uploadArticleImg();
@@ -50,6 +66,87 @@ class AdminArticlesController extends MainController
                 $this->userToTwig['template'] = 'admin';
 
                 $this->template = 'admin' . DS . 'post-confirmation.twig';
+            }
+        }
+    }
+
+    /**
+     * Sets params and template to twig, about list of articles:
+     * - all articles, uri : .../adminArticles/liste/tous/page..
+     * - articles from a category, uri : .../adminArticles/liste/category_name/page..
+     *
+     * @param array $params category or all, and page number, given in URL
+     *
+     * @return void
+     */
+    public function liste(array $params): void
+    {
+        if ($this->userToTwig['admin']) {
+            $this->userToTwig['template'] = 'admin';
+
+            $model = new ArticlesModel();
+        
+            $this->currentPage = (int)(explode('-', $params[1]))[1];
+            $this->totalPosts = $model->getCount($params[0]);
+            $this->totalPages = ceil($this->totalPosts / $this->perPage);
+    
+            if ($this->currentPage <= $this->totalPages && explode('-', $params[1])[0] === 'page') {
+                $this->articles = $model->getArticlesList($this->currentPage, $this->perPage, $params[0]);
+                $this->template = 'admin' . DS . 'articles.twig';
+    
+                if ($model->categoryExist($params[0])) {
+                    $this->template = 'admin' . DS . 'articles-by-category.twig';
+                    $this->params['category'] = $params[0];
+                }
+    
+                foreach ($this->articles as $key) {
+                    $key['slug'] = $this->toSlug($key['title']);
+                    array_shift($this->articles);
+                    array_push($this->articles, $key);
+                }
+    
+                $this->params = array_merge($this->params, array(
+                    'page'       => $this->currentPage,
+                    'totalPages' => $this->totalPages,
+                    'articles'   => $this->articles,
+                    'statePrev'  => ($this->currentPage === 1) ? ' disabled' : '',
+                    'stateNext'  => ($this->currentPage === $this->totalPages) ? ' disabled' : '',
+                    'prev'       => ($this->currentPage === 1) ? 1 : $this->currentPage - 1,
+                    'next'       => ($this->currentPage === $this->totalPages) ? $this->totalPages : $this->currentPage + 1
+                ));
+            }
+        }
+    }
+
+    /**
+     * Sets params and template to twig, about single article page
+     *
+     * @param array $params slug given in URL
+     *
+     * @return void
+     */
+    public function numero(array $params): void
+    {
+        if ($this->userToTwig['admin']) {
+            $this->userToTwig['template'] = 'admin';
+
+            $model = new ArticlesModel();
+
+            if ($model->idExist($params[0])) {
+                $slug = $this->toSlug($model->getTitle((int)$params[0]));
+    
+                if ($slug != $params[1]) {
+                    $url = SITE_ADRESS . DS . 'articles' . DS . 'numero' . DS . $params[0] . DS . $slug;
+                    $this->redirect($url);
+                }
+    
+                $this->template = 'admin' . DS . 'single-article.twig';
+                $this->params = $model->getSingleArticle($params[0]);
+    
+                $comment = new CommentModel();
+                if (!empty($comment->readValidated($params[0]))) {
+                    $this->params['comments'] =  $comment->readValidated($params[0]);
+                }
             }
         }
     }
