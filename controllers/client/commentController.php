7f54ec816abb497ded7985875e61acc55176e320
phil-all <allard.latour.philippe@gmail.com>
2021-12-09 21:29:03 +0100

    :hammer: Code climate issue : Refactor comment controller

--- controllers/client/commentController.php
+++ controllers/client/commentController.php
@@ -15,7 +15,42 @@ class CommentController extends MainController
     use \Over_Code\Libraries\Helpers;
 
     /**
-     * Create an article comment from POST
+     * Json Web Token manager
+     *
+     * @var object $jwt
+     */
+    private $jwt;
+
+    /**
+     * User
+     *
+     * @var object $user
+     */
+    private $user;
+
+    /**
+     * JWT token
+     *
+     * @var string $token
+     */
+    private $token;
+
+    /**
+     * JWT token payload
+     *
+     * @var array $payloaod
+     */
+    private $payload;
+
+    /**
+     * Uri parameters
+     *
+     * @var array
+     */
+    private $uriParams;
+
+    /**
+     * Create an article comment from POST.
      *
      * @param array $params
      *
@@ -23,43 +58,104 @@ class CommentController extends MainController
      */
     public function post(array $params): void
     {
-        $paramsTest = count($params) === 1 && $params[0] === $this->getCOOKIE('CSRF');
+        $this->uriParams = $params;
 
-        if ($paramsTest) {
-            $articleParam = preg_replace(
-                '~' . SINGLE_ARTICLE . '~',
-                '',
-                $this->getSERVER('HTTP_REFERER')
-            );
+        $this->getToken();
 
-            $token = (empty($this->getCOOKIE('token'))) ? '' : $this->getCOOKIE('token');
+        $this->jwt = new Jwt();
 
-            $jwt = new Jwt();
-            if ($jwt->isJWT($token) && $jwt->isSignatureCorrect($token)) {
-                $user = new UserModel();
+        $this->user = new UserModel();
+
+        $this->payload = $this->jwt->decodeDatas($this->token, 1);
+
+        if ($this->validator()) {
+            $articleParam = $this->getArticleParams();
+
+            $this->user->hydrate('renewal', $this->payload['email'], 900);
     
-                $payload  = $jwt->decodeDatas($token, 1);
-                $ipLog    = $user->readIpLog($payload['email']);
-                $remoteIp = $this->getSERVER('REMOTE_ADDR');
+            $this->setCOOKIE('token', $this->user->getToken());
+            $this->setCOOKIE('token_obj', 'renewal');
     
-                if ($jwt->isNotExpired($payload) && ($ipLog === $remoteIp)) {
-                    $user->hydrate('renewal', $payload['email'], 900); // exp 15 min
+            $content = $this->getPOST('comment');
+            $serial  = $this->user->getSerial();
+            $article = explode('/', $articleParam)[0];
     
-                    $this->setCOOKIE('token', $user->getToken());
-                    $this->setCOOKIE('token_obj', 'renewal');
-                }
+            $comment = new commentModel();
+            $comment->create($content, $serial, $article);
     
-                $content = $this->getPOST('comment');
-                $serial  = $user->getSerial();
-                $article = explode('/', $articleParam)[0];
-    
-                $comment = new commentModel();
-                $comment->create($content, $serial, $article);
-    
-                $this->redirect(SINGLE_ARTICLE . $articleParam . "/#comment");
-            }
+            $this->redirect(SINGLE_ARTICLE . $articleParam . "/#comment");
         }
         
         $this->redirect(SITE_ADRESS);
+    }
+
+    /**
+     * Gets cookie token end set token attribute.
+     *
+     * @return void
+     */
+    private function getToken(): void
+    {
+        $this->token = (empty($this->getCOOKIE('token'))) ? '' : $this->getCOOKIE('token');
+    }
+
+    /**
+     * Checks uri parameters.
+     *
+     * @return boolean
+     */
+    private function isUriValid(): bool
+    {
+        return count($this->uriParams) === 1 &&
+            $this->uriParams[0] === $this->getCOOKIE('CSRF');
+    }
+
+    /**
+     * Gets parameters regarding article from the HTTP referer uri.
+     *
+     * @return string
+     */
+    private function getArticleParams(): string
+    {
+        return preg_replace(
+            '~' . SINGLE_ARTICLE . '~',
+            '',
+            $this->getSERVER('HTTP_REFERER')
+        );
+    }
+
+    /**
+     * Checks token and IP adress.
+     *
+     * @return boolean
+     */
+    private function validator(): bool
+    {
+        return $this->tokenTest() && $this->ipTest() && $this->isUriValid();
+    }
+    
+    /**
+     * Checks if token is valid.
+     *
+     * @return boolean
+     */
+    private function tokenTest(): bool
+    {
+        return $this->jwt->isJWT($this->token) &&
+            $this->jwt->isSignatureCorrect($this->token) &&
+            $this->jwt->isNotExpired($this->payload);
+    }
+
+    /**
+     * Verifies if user IP correspond to token stored IP.
+     *
+     * @return boolean
+     */
+    private function ipTest(): bool
+    {
+        $ipLog    = $this->user->readIpLog($this->payload['email']);
+        $remoteIp = $this->getSERVER('REMOTE_ADDR');
+
+        return $ipLog === $remoteIp;
     }
 }
